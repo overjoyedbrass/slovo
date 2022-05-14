665dd6f5cdca6b32ca45644ae36e86937de7b87a
overjoyedbrass <79214190+overjoyedbrass@users.noreply.github.com>
2022-05-15 01:42:26 +0200

    new dictionary source

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,15 +1,16 @@
 import React from 'react'
-
 import { useSelector } from 'react-redux'
 import { selectCurrentTheme } from '../theme/themeSlice'
 import { themes } from '../theme/themes'
+
+const URL = "https://slovnik.juls.savba.sk/?w=";
 
 export const InfoBox = ({targetWord}) => {
     const theme = themes[useSelector(selectCurrentTheme)]
     return (
         <div className="infoBox">
             Hľadané slovo je <a 
-                href={`https://slovnik.aktuality.sk/pravopis/?q=${targetWord}`}
+                href={`${URL}${targetWord}`}
                 target="blank"
                 style={{color: theme.href, backgroundColor: theme.bgColor}}
             >{targetWord.toUpperCase()}</a>
